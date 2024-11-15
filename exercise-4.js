f98b17d789396557120c71fda6e74434853a636e
pangphaichit <pg_grj@hotmail.com>
2024-11-15 14:17:26 +0700

    add: submit assignment

---
+++ exercise-4.js
@@ -0,0 +1,24 @@
+// ให้ Query ข้อมูล Document ทั้งหมดที่มีการสั่งจำนวน Pizza น้อยกว่า 5 ถาด และจ่ายเงินด้วย Credit Card ของ mastercard
+
+db.pizzaOrders.find({
+    quantity: { $lt: 5 },                   
+    credit_card_type: "mastercard"          
+})
+
+// Query ข้อมูล Document ทั้งหมดที่สั่งซื้อ Pizza ขนาดเล็ก และมีจำนวนถาดอยู่ระหว่าง 1 - 5 ถาด
+
+db.pizzaOrders.find({
+    $and: [
+        { quantity: { $gte: 1 } },
+        { quantity: { $lte: 5 } }
+        ],                   
+    size: "small"          
+})
+
+// Query ข้อมูล Document ทั้งหมดที่มีการสั่ง Pizza จำนวนมากกว่า 10 ถาด และไม่ได้จ่ายเงินด้วย Credit Card
+
+db.pizzaOrders.find({
+    quantity: { $gte: 10 },                   
+    credit_card_type: null        
+})
+
